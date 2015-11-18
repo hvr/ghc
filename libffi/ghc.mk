c5c00f30abff23b971bec80389b92f52cdba09a6
Herbert Valerio Riedel <hvr@gnu.org>
2015-11-18 12:47:37 +0100

    Fix broken build-system when libffi uses install-sh
    Summary: This hopefully fixes #11109
    
    Reviewers: bgamari, austin
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D1494
    
    GHC Trac Issues: #11109

--- libffi/ghc.mk
+++ libffi/ghc.mk
@@ -88,6 +88,10 @@ $(libffi_STAMP_CONFIGURE): $(TOUCH_DEP)
 	mv libffi/build/ltmain.sh libffi/build/ltmain.sh.orig
 	sed 's#cmd //c echo "\$$1"#cmd /c "echo $$1"#' < libffi/build/ltmain.sh.orig > libffi/build/ltmain.sh
 
+	# See GHC #11109
+	mv libffi/build/Makefile.in libffi/build/Makefile.in.orig
+	sed 's:@INSTALL@:$$(subst ../install-sh,$(TOP)/install-sh,@INSTALL@):g' < libffi/build/Makefile.in.orig > libffi/build/Makefile.in
+
 # * Because -Werror may be in SRC_CC_OPTS/SRC_LD_OPTS, we need to turn
 #   warnings off or the compilation of libffi might fail due to warnings;
 #   hence the -w flags.
