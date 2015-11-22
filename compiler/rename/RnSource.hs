f7172d1fa6a1149aae9d6ba0db3ed0258236f7d8
Herbert Valerio Riedel <hvr@gnu.org>
2015-11-22 15:47:47 +0100

    Implement new `-fwarn-noncanonical-monad-instances`
    TODO: tests, user's guide, better warning-text
    
    Warn about incoherent/non-canonical 'Applicative'/'Monad' instance
    declarations. Specifically the following invariants are checked:
    
    In 'Monad' instances declarations warn if the any of the following
    conditions does not hold:
    
     * If `return` is overridden it must be canonical (i.e. `return = pure`).
     * If `(>>)` is overridden it must be canonical (i.e. `(>>) = (*>)`).
    
    In 'Applicative' instance declarations:
    
     * Warn if 'pure' is defined backwards (i.e. `pure = return`).
     * Warn if '(*>)' is defined backwards (i.e. `(*>) = (>>)`).

--- compiler/rename/RnSource.hs
+++ compiler/rename/RnSource.hs
@@ -30,7 +30,9 @@ import ForeignCall      ( CCallTarget(..) )
 import Module
 import HscTypes         ( Warnings(..), plusWarns )
 import Class            ( FunDep )
-import PrelNames        ( isUnboundName )
+import PrelNames        ( applicativeClassName, pureAName, thenAName
+                        , monadClassName, returnMName, thenMName
+                        , isUnboundName )
 import Name
 import NameSet
 import NameEnv
@@ -449,6 +451,57 @@ rnSrcInstDecl (ClsInstD { cid_inst = cid })
   = do { (cid', fvs) <- rnClsInstDecl cid
        ; return (ClsInstD { cid_inst = cid' }, fvs) }
 
+-- | Warn about unsound/non-canonical 'Applicative'/'Monad' instance
+-- declarations. Specifically, the following conditions are verified:
+--
+-- In 'Monad' instances declarations:
+--
+--  * If 'return' is overridden it must be canonical (i.e. @return = pure@).
+--  * If '(>>)' is overridden it must be canonical (i.e. @(>>) = (*>)@).
+--
+-- In 'Applicative' instance declarations:
+--
+--  * Warn if 'pure' is defined backwards (i.e. @pure = return@).
+--  * Warn if '(*>)' is defined backwards (i.e. @(*>) = (>>)@).
+--
+checkCanonicalMonadInstances :: Name -> LHsBinds Name -> RnM ()
+checkCanonicalMonadInstances cls mbinds
+  | cls == applicativeClassName  = do
+      forM_ (bagToList mbinds) $ \mbind -> do
+          case mbind of
+              L loc FunBind { fun_id = L _ name, fun_matches = mg }
+                  | name == pureAName, isAliasMG mg == Just returnMName
+                  -> addWarnAt loc (text "pure = return")
+
+                  | name == thenAName, isAliasMG mg == Just thenMName
+                  -> addWarnAt loc (text "(*>) = (>>)")
+
+              _ -> pure ()
+
+  | cls == monadClassName  = do
+      forM_ (bagToList mbinds) $ \mbind -> do
+          case mbind of
+              L loc FunBind { fun_id = L _ name, fun_matches = mg }
+                  | name == returnMName, isAliasMG mg /= Just pureAName
+                  -> addWarnAt loc (text "return /= pure")
+
+                  | name == thenMName, isAliasMG mg /= Just thenAName
+                  -> addWarnAt loc (text "(>>) /= (*>)")
+
+              _ -> pure ()
+
+  | otherwise = pure ()
+  where
+    -- | test whether MatchGroup represents a trivial
+    -- "lhsName = rhsName" binding, and return 'Just rhsName' if this is the case
+    isAliasMG :: MatchGroup Name (LHsExpr Name) -> Maybe Name
+    isAliasMG MG {mg_alts = L _ [L _ (Match { m_pats = [], m_grhss = grhss })]}
+        | GRHSs [L _ (GRHS [] body)] lbinds <- grhss
+        , L _ EmptyLocalBinds <- lbinds
+        , L _ (HsVar rhsName) <- body  = Just rhsName
+    isAliasMG _ = Nothing
+
+
 rnClsInstDecl :: ClsInstDecl RdrName -> RnM (ClsInstDecl Name, FreeVars)
 rnClsInstDecl (ClsInstDecl { cid_poly_ty = inst_ty, cid_binds = mbinds
                            , cid_sigs = uprags, cid_tyfam_insts = ats
@@ -472,6 +525,9 @@ rnClsInstDecl (ClsInstDecl { cid_poly_ty = inst_ty, cid_binds = mbinds
         -- (Slightly strangely) when scoped type variables are on, the
         -- forall-d tyvars scope over the method bindings too
        ; (mbinds', uprags', meth_fvs) <- rnMethodBinds False cls ktv_names mbinds uprags
+
+       ; whenWOptM Opt_WarnNonCanonicalMonadInstances $
+         checkCanonicalMonadInstances cls mbinds'
 
        -- Rename the associated types, and type signatures
        -- Both need to have the instance type variables in scope
